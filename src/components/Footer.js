3339f3d03346de049b06a4baa707f979d85d4dd8
Felipe Cayres <felipescayres@gmail.com>
2024-10-30 13:42:25 -0400

    design[footer]: added footer message to explain images are only for illustration purposes, and may not be real

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container">
-        <p className="text">&copy; {new Date().getFullYear()}</p>
-
+        <p className="text">
+          The images shows are for illustrational purposes and may not be an
+          exact repersentation of the product.
+        </p>
         <p className="text">Enjoy your drinks and please drink responsibly.</p>
       </div>
     </footer>
